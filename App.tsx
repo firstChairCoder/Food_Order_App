421a419d7d8067b7c193ebbd322e47e36e8f69d3
firstChairCoder <md1qq7@gmail.com>
2022-04-08 07:08:36 +0100

    Config + comments removal.

--- App.tsx
+++ App.tsx
@@ -20,9 +20,9 @@ const Stack = createNativeStackNavigator<Routes>();
 
 const HomeTab = createBottomTabNavigator();
 
-function Home() {
+function Homed() {
   return (
-    <HomeTab.Navigator>
+    <HomeTab.Navigator screenOptions={{ headerShown: false }}>
       <HomeTab.Screen
         name={"Home"}
         component={HomeScreen}
@@ -85,44 +85,9 @@ export default function App() {
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
           <Stack.Screen name={"Landing"} component={LandingScreen} />
-          <Stack.Screen name={"Home"} component={Home} />
+          <Stack.Screen name={"Home"} component={Homed} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
 }
-
-// import React, { useState, useEffect } from "react";
-// import { View, Text } from "react-native";
-// import * as Location from "expo-location";
-
-// export default function App() {
-//   const [location, setLocation] = useState(null);
-//   const [errorMsg, setErrorMsg] = useState(null);
-
-//   useEffect(() => {
-//     (async () => {
-//       const { status } = await Location.requestForegroundPermissionsAsync();
-//       if (status !== "granted") {
-//         setErrorMsg("Permission to access location was denied");
-//         return;
-//       }
-
-//       const location = await Location.getCurrentPositionAsync({});
-//       setLocation(location);
-//     })();
-//   }, []);
-
-//   let text = "Waiting...";
-//   if (errorMsg) {
-//     text = errorMsg;
-//   } else if (location) {
-//     text = JSON.stringify(location);
-//   }
-
-//   return (
-//     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-//       <Text>{text}</Text>
-//     </View>
-//   );
-// }
